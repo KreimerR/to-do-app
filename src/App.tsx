650fb19d94526661ffb62d2ef47539dba745a1e9
Rodion Kreimer <kreimerrodion@gmail.com>
2025-05-19 21:08:59 +0300

    Made a UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@ import Task from "./components/Task.tsx"
 
 export default function App() {
     return (
-        <>
+        <div className="to-do-app">
             <h1 className="name">TODO LIST</h1>
+
             <div className="buttons-container">
-                <button className="task-btn">Add Task</button>
+                <button className="task main-btn">Add Task</button>
                 
                 <div className="right-main-btns-container">
-                    <button className="all-sort-btn">All</button>
-                    <button className="pending-sort-btn">Pending</button>
-                    <button className="completed-sort-btn">Completed</button>
+                    <button className="all active sort-btn main-btn">All</button>
+                    <button className="pending sort-btn main-btn">Pending</button>
+                    <button className="completed sort-btn main-btn">Completed</button>
                 </div>
             </div>
+
             <div className="tasks-container">
                 <Task />
                 <Task />
                 <Task />
             </div>
-        </>
+        </div>
     )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,17 +1,38 @@
+import { useState } from "react"
+import checkmark from "../assets/check.png"
+import trashBin from "../assets/trash-bin.png"
+import pencil from "../assets/pencil.png"
+
 export default function Task() {
+    const [isChecked, setIsChecked] = useState(false)
+
+    function toggleCheckmark() {
+        setIsChecked(prevIsChecked => !prevIsChecked)
+    }
+    
     return (
         <div className="task-container">
             <div className="left-task-container">
-                <button className="check unchecked"></button>
+                {isChecked ? 
+                    <button onClick={toggleCheckmark} className="check checked">
+                        <img className="icon" src={checkmark} alt="checkmark" />
+                    </button> :
+                    <button onClick={toggleCheckmark} className="check unchecked"></button>
+                }
+
                 <div className="info-container">
-                    <h2>Task name</h2>
+                    <h3>Task name</h3>
                     <p>5:23 AM, 01/06/2022</p>
                 </div>                
             </div>
 
             <div className="right-task-container">
-                <button className="trash-btn"></button>
-                <button className="edit-btn"></button>
+                <button className="trash-btn">
+                    <img className="icon" src={trashBin} alt="delete" />
+                </button>
+                <button className="edit-btn">
+                    <img className="icon" src={pencil} alt="edit" />
+                </button>
             </div>
         </div>
     )
